c02080b134a501147562e82f8d01a46192772b5f
AbdelrahmanHashem468 <ahmedabdelrahman468@gmail.com>
2020-02-17 04:16:28 +0200

    Authantication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fetchedData=$request->all();
+        $this->validate($request, [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'phonenumber'=>'required',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phonenumber'=>$request->phonenumber,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('TutsForWeb')->accessToken;
+        
+        return response()->json(['token' => $token], 200);
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('TutsForWeb')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'UnAuthorised'], 401);
+        }
+    }
+}
