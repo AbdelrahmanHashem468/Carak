5759d243db612bc6982e05c91900e0c86915c3c7
AbdelrahmanHashem468 <ahmedabdelrahman468@gmail.com>
2020-03-06 01:41:15 +0200

    finshpartOne

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    <?php echo csrf_token();?>
                 </div>
 
                 <div class="links">
