77407e1ea1e712271b0243ff43b1cc43754f21e7
AbdelrahmanHashem468 <ahmedabdelrahman468@gmail.com>
2020-03-24 23:36:57 +0200

    editprice

--- app/Model/Car/Car_Price.php
+++ app/Model/Car/Car_Price.php
@@ -14,15 +14,25 @@ class Car_Price extends Model
 
     public static function getAllCarPrice()
     {
-        $carsPrices = Car::getCarModel();
+        $carsPrices = Car::getAllCars();
         foreach($carsPrices as $carsPrice)
+        {
+            $carsPrice['car_price']=$carsPrice->car_price;
+            foreach($carsPrice['car_price'] as $car)
+            {
+                $car['car_model_name']=$car->car_model->name;
+                unset($car['car_model']);
+            }
+        }
+        return $carsPrices;
+        /*foreach($carsPrices as $carsPrice)
         {
             foreach($carsPrice['car_models'] as $car)
             {
                 $car['car_price'] =$car->car_price;
             }
-        }
-        return $carsPrices;
+        }*/
+        
     }
 
     public function car()
